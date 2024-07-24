3e2493aaee34e013389bb0a7450331be03c19333
Devesh Kumar Singh <deveshsingh7909@gmail.com>
2024-07-24 23:14:34 +0530

    Directional_Drilling.py

--- Directional_Drilling.py
+++ Directional_Drilling.py
@@ -8,18 +8,6 @@ from math import pi
 import sympy as sp
 from PIL import Image
 
-st.markdown(
-    """
-    <style>
-    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
-    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
-    .viewerBadge_text__1JaDK {
-        display: none;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
 
 # Initialize session state for navigation
 if 'page' not in st.session_state:
