12b7f6f26adfbe727a1bf94e0e4d1de2b9f202a7
Devesh Kumar Singh <deveshsingh7909@gmail.com>
2024-07-26 20:11:12 +0530

    Directional_Drilling.py

--- Directional_Drilling.py
+++ Directional_Drilling.py
@@ -8,10 +8,10 @@ from math import pi
 import sympy as sp
 from PIL import Image
 
-hide_github_icon = “”"
+hide_github_icon = """
     
     .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob, .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137, .viewerBadge_text__1JaDK{ display: none; } #MainMenu{ visibility: hidden; } footer { visibility: hidden; } header { visibility: hidden; }
-“”"
+"""
 st.markdown(hide_github_icon, unsafe_allow_html=True)
 # Initialize session state for navigation
 if 'page' not in st.session_state:
