fbec04932275b32fd1a84f5c5e00f86946c1dbc9
Devesh Kumar Singh <deveshsingh7909@gmail.com>
2024-07-26 20:36:00 +0530

    Directional_Drilling.py

--- Directional_Drilling.py
+++ Directional_Drilling.py
@@ -8,13 +8,13 @@ from math import pi
 import sympy as sp
 from PIL import Image
 
-st.markdown(   
-    "<style>.css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,.styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,.viewerBadge_text__1JaDK
-         {
-        display: none;
-         }</style>",
-    unsafe_allow_html=True
-)
+
+hide_github_icon = """
+     #stActionButtonIcon {
+         visibility: hidden;
+     }
+"""
+st.markdown(hide_github_icon, unsafe_allow_html=True)
 
 # <div data-testid="stActionButtonIcon" class="st-emotion-cache-q16mip e3g6aar1"></div>
 # <div data-testid="stActionButtonIcon" class="st-emotion-cache-q16mip e3g6aar1"></div>
